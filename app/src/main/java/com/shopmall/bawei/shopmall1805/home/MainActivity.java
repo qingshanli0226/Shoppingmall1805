cf43cb9f270d576f9eecb243a25eafe6b4106aa6
liqingshan <liqingshan@sina.cn>
2020-11-19 19:02:17 +0800

    base

---
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/home/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shopmall.bawei.shopmall1805.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shopmall.bawei.shopmall1805.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
