0e3db7f4b79dc20b9ed92ea265d15a87b1652873
“yjw” <1285597214@qq.com>
2020-11-20 10:55:41 +0800

    Yoyo

--- app/src/main/java/com/shopmall/bawei/shopmall1805/home/MainActivity.java
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/home/MainActivity.java
@@ -2,6 +2,7 @@ package com.shopmall.bawei.shopmall1805.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.shopmall.bawei.shopmall1805.R;
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "aaaaaaa", Toast.LENGTH_SHORT).show();
     }
 }
