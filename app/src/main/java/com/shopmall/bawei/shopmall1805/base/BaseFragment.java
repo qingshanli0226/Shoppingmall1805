17bfa001dff1e589d01386096239f98ddf79e8c6
严东明 <2328934408@qq.com>
2020-11-20 15:09:35 +0800

    "修改了广告页面"

---
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/base/BaseFragment.java
@@ -0,0 +1,42 @@
+package com.shopmall.bawei.shopmall1805.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+
+    public Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context =getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+    protected abstract View initView();
+
+
+}
