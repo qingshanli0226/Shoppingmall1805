7bb380dae554ee16533f3f362d146b0a8cd44e79
严东明 <2328934408@qq.com>
2020-11-20 11:28:12 +0800

    "修改了广告页面"

---
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/home/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.shopmall.bawei.shopmall1805.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shopmall.bawei.shopmall1805.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
