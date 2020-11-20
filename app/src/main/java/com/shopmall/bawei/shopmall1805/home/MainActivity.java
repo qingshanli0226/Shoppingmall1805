7b3f7aca871d9f44ac75ee28cb8f64cb2af4df61
123wk <1933730971@qq.com>
2020-11-20 10:53:34 +0800

    log打印

--- app/src/main/java/com/shopmall/bawei/shopmall1805/home/MainActivity.java
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/home/MainActivity.java
@@ -2,6 +2,7 @@ package com.shopmall.bawei.shopmall1805.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.shopmall.bawei.shopmall1805.R;
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.w("123","451331313");
     }
 }
