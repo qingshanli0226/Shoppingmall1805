c34c886195fbfb0e2e2222c956b443c944868445
lk <2723814361@qq.com>
2020-11-20 14:08:27 +0800

    app

--- app/src/main/java/com/shopmall/bawei/shopmall1805/app/ShopMallApplication.java
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/app/ShopMallApplication.java
@@ -1,8 +1,8 @@
-package com.shopmall.bawei.shopmall1805;
+package com.shopmall.bawei.shopmall1805.app;
 
 import android.app.Application;
 
-public class ShopmallApplication extends Application {
+public class ShopMallApplication extends Application {
 
     @Override
     public void onCreate() {
