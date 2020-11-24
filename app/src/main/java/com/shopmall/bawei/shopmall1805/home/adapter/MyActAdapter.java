00412ce258c7e6f08331effd54dc3b9e2da5e3f1
严东明 <2328934408@qq.com>
2020-11-24 13:57:47 +0800

    ""

---
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/home/adapter/MyActAdapter.java
@@ -0,0 +1,33 @@
+package com.shopmall.bawei.shopmall1805.home.adapter;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bw.net.Contants;
+import com.bw.net.bean.HomeFragmentBean;
+import com.shopmall.bawei.shopmall1805.R;
+import com.shopmall.bawei.shopmall1805.base.BaseAdapter;
+
+import java.util.List;
+
+public class MyActAdapter extends BaseAdapter<HomeFragmentBean.ResultBean.ActInfoBean> {
+    @Override
+    protected int getLayoutId(int viewType) {
+        return R.layout.item_act_data;
+    }
+
+    @Override
+    protected void convert(BaseViewHoder baseViewHoder, int viewType, HomeFragmentBean.ResultBean.ActInfoBean actInfoBean) {
+        ImageView imageView = baseViewHoder.itemView.findViewById(R.id.act_image);
+
+        Glide.with(baseViewHoder.itemView.getContext()).load(Contants.BASE_URl_IMAGE+actInfoBean.getIcon_url()).into(imageView);
+
+    }
+
+
+
+    @Override
+    public int getViewType(int position) {
+        return position;
+    }
+}
