552970a0dd5b4c6abcc3ae8a4d4f0c8b900491df
MK16A1 <473992528@qq.com>
2020-11-26 13:27:42 +0800

    登录注册，bug未解决

---
+++ app/src/main/java/com/shopmall/bawei/shopmall1805/WelcomActivity.java
@@ -0,0 +1,16 @@
+package com.shopmall.bawei.shopmall1805;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WelcomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
