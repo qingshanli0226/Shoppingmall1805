0b303ad78cd4f37a3586f339bdf2b74a2eaf395d
“wftxt” <778411131@example.com>
2020-11-26 13:44:13 +0800

    首页的布局实现

--- net/src/main/java/com/example/net/MessageBean.java
+++ net/src/main/java/com/example/net/MessageBean.java
@@ -1,4 +1,19 @@
 package com.example.net;
 
 public class MessageBean {
+
+    private String message;
+
+
+    public MessageBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- user/src/main/java/com/shopmall/bawei/user/contract/RegisterContract.java
+++ user/src/main/java/com/shopmall/bawei/user/contract/RegisterContract.java
@@ -1,4 +1,18 @@
 package com.shopmall.bawei.user.contract;
 
-public class RegisterContract {
+import com.example.framework.BaseIPresenter;
+import com.example.framework.IVIew;
+import com.example.net.JavaBean;
+
+public interface RegisterContract {
+
+    interface RegisterView extends IVIew {
+        void getViewData1(JavaBean clotheslist);
+    }
+
+    abstract class RegisterPresenter extends BaseIPresenter<RegisterContract.RegisterView> {
+
+        public abstract void getData1();
+    }
+
 }
